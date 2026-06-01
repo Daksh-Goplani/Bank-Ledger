897d6dd5352a1c1b50496f81a27180bac5378b84
Daksh Goplani <dakshgoplani7@gmail.com>
2026-06-01 21:59:49 +0530

    Add user logout functionality and enhance token validation in auth middleware

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model')
 const jwt = require('jsonwebtoken')
 const emailService = require('../services/email.service')
+const tokenBlacklistModel = require("../models/blackList.model")
 
 /** 
  * - user register controller 
@@ -70,4 +71,24 @@ async function userLoginController(req, res) {
     })
 }
 
-module.exports = { userRegisterController, userLoginController }
+/**
+ * - User Logout Controller
+ * - POST /api/auth/logout
+ */
+async function userLogoutController(req, res) {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1]
+    if(!token){
+        return res.status(200).json({
+            message: "Already logged out"
+        })
+    }
+    res.cookie("token","")
+    await tokenBlacklistModel.create({
+        token
+    })
+    return res.status(200).json({
+        message:"User logged out successfully"
+    })
+}
+
+module.exports = { userRegisterController, userLoginController, userLogoutController }
